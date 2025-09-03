d2704e0d2ce024f94aa80323f4d0e4ebd3ccbcb3
Gabrieli Arpini <Gabrieli.arpini@icloud.com>
2025-09-03 20:40:31 -0300

    chore(backend): adiciona injeção de dependencia entre as camadas de api, infra e aplicação.

---
+++ backend/MeuCorre.Infra/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using MeuCorre.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeuCorre.Infra
+{
+    public static class DependencyInjection
+    {
+         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Mysql");
+
+            services.AddDbContext<MeuDbContext>(options => options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
+
+            return services;
+
+        }
+    }
+}
